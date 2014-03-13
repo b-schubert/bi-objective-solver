9499f6454ed9d00968f62cba3578cfb657446b2e
b-schubert <schubert@informatik.uni-tuebingen.de>
2014-03-13 17:14:31 +0100

    NormalConstraint implementation untested

--- cplex_impl.py
+++ cplex_impl.py
@@ -6,7 +6,7 @@ from concurrent.rectangle_manager import RectangleSplittingManager
 from algorithms.BiDirectionalEpsilonConstraint import DoubleEpsilonConstraintSolver
 from algorithms.EpsilonConstraint import EpsilonConstraintSolver
 from algorithms.RectangleSplitting import RectangleSplittingSolver
-
+from algorithms.NormalConstraint import NormalConstraint
 
 def rectangle():
     lp1 = sys.argv[1]
@@ -26,6 +26,16 @@ def epsilon():
     z2 = cplex.Cplex(lp2)
     rectangle = EpsilonConstraintSolver(z1, z2,  ["x", "y"])
     return rectangle.solve()
+
+
+def normal_constraint():
+    lp1 = sys.argv[1]
+    lp2 = sys.argv[2]
+
+    z1 = cplex.Cplex(lp1)
+    z2 = cplex.Cplex(lp2)
+    nc = NormalConstraint(z1, z2, ["x", "y"])
+    return nc.solve(4)
 
 
 def double_epsilon():
@@ -53,6 +63,17 @@ if __name__ == "__main__":
 
     t = time.time()
     sols = m.approximate(0.05)
+    sols = normal_constraint()
+    e = time.time()
+    print "Concurrent New Rectangle Runtime: ", e-t
+    for s in sols:
+    #    #print s.vars
+        print s.objs
+
+    sys.exit()
+
+    t = time.time()
+    sols = concurrent_rectangle()
     e = time.time()
     print "Concurrent New Rectangle Runtime: ", e-t
     for s in sols:
