8d1ff35d409805fe073b80f1251adfc36d0edd28
b-schubert <schubert@informatik.uni-tuebingen.de>
2014-05-14 11:09:36 -0400

    deleted code fragments not used

--- cplex_impl.py
+++ cplex_impl.py
@@ -161,66 +161,6 @@ if __name__ == "__main__":
     plot_pareto_front("../test_pareto_front.png", map(lambda x: x.objs[0],sols),map(lambda x: x.objs[1],sols))
 
 
-    c = 0
-    xes = []
-    yes = []
-    for s in sols:
-    #    #print s.vars
-        print s.objs
-
-
-        seq = filter(lambda x: "x" in x, s.vars)
-
-        #print seq
-        epi = filter(lambda x: "y" in x[0], s.vars.items())
-        #print epi
-        wt = "EVRNPCACFRNYVPVCGSDGKTYGNPCMLNCAAQTKVPGLKLVHEGRCQRSNVEQF"
-        mutant = ["A"]*len(wt)
-        ys = [0]*len(wt)
-        epitopes = {}
-        for x in seq:
-            pos,aa =  re.match("x\((.*),(.*)\)",x).group(1,2)
-            mutant[int(pos)-1] = aa
-        for y in epi:
-            print y, y[0],y[1]
-            allele,pos =  re.match("y\((.*),(.*)\)",y[0]).group(1,2)
-            pos = int(pos)-1
-            epitopes.setdefault(allele,[]).append(pos)
-            for i in xrange(9):
-                ys[pos+i] += float(y[1])
-
-        print "Ys", ys
-        xes.append(list(mutant))
-        yes.append(ys)
-        seq_len = len(wt)
-        plot_variable_distribution("../test_variables_correlation_%i.png"%c, list(mutant), ys)
-        print "mt       : "+"".join(mutant[i] for i in xrange(seq_len))
-        c+=1
-        seq_len = len(wt)
-        print "Hemming Distance: "+str(sum( wt[i] != mutant[i] for i in range(seq_len) ))+ str([ (i+1,wt[i],mutant[i]) for i in range(seq_len) if wt[i] != mutant[i]])
-        print "wt       : "+wt
-        print "mt       : "+"".join(mutant[i] for i in xrange(seq_len))
-
-        for allele, ep_pos in sorted(epitopes.items()):
-                    st = allele+": "
-                    i=0
-
-                    while i < seq_len:
-                        if i in epitopes[allele]:
-                            end = i+9 if i+9 <= seq_len else seq_len
-                            st+="".join( "^" if wt[j] != mutant[j] else "*"  for j in xrange(i,end) )
-                            i+=9
-                        elif wt[i] != mutant[i]:
-                            st+="^"
-                            i+=1
-                        else:
-                            st+="-"
-                            i+=1
-                    print st
-    plot_3d_variable_distribution("../test_3d_var_dist.png", xes, yes)
-    sys.exit()
-
-
     m = concurrent_rectangle()
     t = time.time()
     sols = m.approximate(0.01)
